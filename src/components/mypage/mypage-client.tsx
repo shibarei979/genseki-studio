d04c11ab1f56b303f92ef9d6e2a19afdd2d75139
shibarei979 <shibarei979@gmail.com>
2026-09-03 17:28:00 +0900

    feat: show typo reports to the author in mypage

--- src/components/mypage/mypage-client.tsx
+++ src/components/mypage/mypage-client.tsx
@@ -1815,7 +1815,7 @@ export default function MypageClient({
   }
 
   const tabContent: Record<Tab, React.ReactNode> = {
-    mypage:'', works:'', bookmarks:'', history:'', tweet:'', mission:'', settings:'', series:'',
+    mypage:'', works:'', typos:'', bookmarks:'', history:'', tweet:'', mission:'', settings:'', series:'',
   }
 
   return (
